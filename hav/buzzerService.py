6f84e53e556d7bec215a74047eec860cc5c0b117
Rsamaz <ruben.sangil@hotmail.com>
2022-07-17 18:46:24 +0200

    pruebas

--- hav/buzzerService.py
+++ hav/buzzerService.py
@@ -1,5 +1,5 @@
 #################################################################################
-#               Proyecto:   buzzerService                                          #
+#               Proyecto:   buzzerService                                       #
 #               Autor: Oscar Loras Delgado                                      #
 #                                                                               #
 #################################################################################
@@ -8,12 +8,8 @@ import time
 import logging
 import ConfigHelper
 import RPi.GPIO as GPIO
-import ruleService
-buzzerPin = 26
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(buzzerPin, GPIO.OUT, initial=GPIO.LOW)
+import BuzzerHelper.Buzzer as Buzzer
+from hav.BuzzerHelper.Buzzer import *
 
 #Creacion del loger para los datos cientificos
 logger = logging.getLogger('server_logger')
@@ -33,7 +29,7 @@ formatterInformer = logging.Formatter('[%(asctime)s][%(levelname)s][%(message)s]
 inf.setFormatter(formatterInformer)
 loggerLog.addHandler(inf)
 
-
+sensor = Buzzer()
 act = ConfigHelper.isBuzzerActivo()
 tiempoMuestreoBuzzer = ConfigHelper.getTiempoMuestreoBuzzer()
 
@@ -46,29 +42,15 @@ if act == 1:
 		try:
 			tiempoMuestreoBuzzer = ConfigHelper.getToken("Buzzer", "tiempoMuestreoBuzzer", tiempoMuestreoBuzzer)
 
-			def on():
-				GPIO.output(buzzerPin,GPIO.HIGH)
-				print ("Buzzer ON - Emitiendo sonido...")
-				time.sleep(1) # Delay in seconds
-			
-			#ruleService.thresholdOp:GPS.0.lt.10000:buzzer.on
-			def off():
-				GPIO.output(buzzerPin,GPIO.LOW)
-				print ("Buzzer OFF - No sound")
-				
-
-			def timeOn():
-				time.perf_counter(buzzerPin,GPIO.HIGH)
-				print("Tiempo que lleva sonando la sonda: ")
+			buzzerStatus = sensor.getBuzzerDataFromFile()
+			buzzerInitial = sensor.initialCheck()
+			#thresholdOp:GPS.0.lt.10000:buzzer.on
 			
 
             #INICIO: Espacio para recuperar los datos del sensor a partir de la libreria
-			buzzerTimer = timeOn()
-			buzzerOn = on()
-			buzzerOff = off()
 			
             #Escritura de datos en el archivo de datos del sensor. Todo lo que se escriba aqui sera lo que potencialmente se acabe enviando por telemetria.
-			logger.info(time(buzzerTimer))
+			logger.info(str(round(buzzerStatus,2)) + "|" + str(round(buzzerInitial,4)))
 
                         #FINAL: Espacio para recuperar los datos del sensor a partir de la libreria
 			time.sleep(tiempoMuestreoBuzzer)
