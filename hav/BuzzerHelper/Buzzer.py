e673ee01e92accc3d1c8944cdda292391c19ca26
Rsamaz <ruben.sangil@hotmail.com>
2022-07-20 19:21:17 +0200

    añadir BuzzerHelperUtil

--- hav/BuzzerHelper/Buzzer.py
+++ hav/BuzzerHelper/Buzzer.py
@@ -1,24 +1,10 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO # pylint: disable=import-error
 import time
 buzzerPin = 26
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(buzzerPin, GPIO.OUT, initial=GPIO.LOW)
-#import RPi.GPIO as GPIO
-
-def getBuzzerDataFromFile():
-        #logger.info(str(round(buzzerStatus,2)) + "|" + str(round(buzzerInitial,4)))
-        buzzerData = [str(0), str(0)]
-        try:
-                with open('/data/hab_sonda/logs/buzzerdata.log') as buzzerdatafile:
-                        line = list(buzzerdatafile)[-1]
-                bdata = line.split('|')
-                buzzerData[0] = str(bdata[1])
-                buzzerData[1] = int(bdata[2])
-                return buzzerData
-        except:
-                return buzzerData
 
 def initialCheck():
 	GPIO.output(buzzerPin,GPIO.HIGH)
