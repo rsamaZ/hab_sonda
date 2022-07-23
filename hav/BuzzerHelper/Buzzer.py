9995f6522917678a619324a5427b6f5c8b243a39
rsamaz <ruben.sangil@hotmail.com>
2022-07-23 23:30:35 +0200

    refactor Buzzer

--- hav/BuzzerHelper/Buzzer.py
+++ hav/BuzzerHelper/Buzzer.py
@@ -1,13 +1,20 @@
-import RPi.GPIO as GPIO # pylint: disable=import-error
+import RPi.GPIO as GPIO
 import time
+
 buzzerPin = 26
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(buzzerPin, GPIO.OUT, initial=GPIO.LOW)
 
-class Buzzer():
-	def initialCheck():
-		GPIO.output(buzzerPin,GPIO.HIGH)
-		print ("Buzzer Armado - Ready and waiting for GPS signal")
-		GPIO.output(buzzerPin,GPIO.LOW)
+
+def initialCheck():
+    GPIO.output(buzzerPin, GPIO.HIGH)
+    time.sleep(0.25)
+    print("Buzzer Armado - Ready and waiting for GPS signal")
+    GPIO.output(buzzerPin, GPIO.LOW)
+
+
+def on():
+    GPIO.output(buzzerPin, GPIO.HIGH)
+    print("Buzzer ON - HAB altitude is under 10000m")
--- hav/BuzzerHelper/BuzzerHelperUtil.py
+++ hav/BuzzerHelper/BuzzerHelperUtil.py
@@ -2,16 +2,15 @@ import time
 import sys
 import os
 
-#Metodo que recupera los datos del UV del archivo de datos del servicio
-def getBuzzerDataFromFile():
-        buzzerData = [str(0),str(0)]
-        try:
-                with open('/data/hab_sonda/logs/buzzerdata.log') as buzzerdatafile:
-                        line = list(buzzerdatafile)[-1]
-                bdata = line.split('|')
-                buzzerData[0] = str(bdata[1])
-                buzzerData[1] = str(bdata[2])
-                return buzzerData
-        except:
-                return buzzerData
 
+def getBuzzerDataFromFile():
+    buzzerData = [str(0), str(0)]
+    try:
+        with open('/data/hab_sonda/logs/buzzerdata.log') as buzzerdatafile:
+            line = list(buzzerdatafile)[-1]
+        bdata = line.split('|')
+        buzzerData[0] = str(bdata[1])
+        buzzerData[1] = str(bdata[2])
+        return buzzerData
+    except:
+        return buzzerData
